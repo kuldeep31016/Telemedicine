4458ffc56dd10d8331de1fb37a2be956bbe344a4
kuldeepraj <kr9667102@gmail.com>
2026-01-31 19:59:31 +0530

    feat: add PlaceholderScreens component for re-exporting consultation-related screens

---
+++ mobile/NabhaApp/src/screens/consultation/PlaceholderScreens.js
@@ -0,0 +1,6 @@
+// Re-export the actual implemented components
+export { default as DocumentUpload } from './DocumentUpload';
+export { default as PersonalDetailsForm } from './PersonalDetailsForm';
+export { default as DoctorSelection } from './DoctorSelection';
+export { default as PaymentGateway } from './PaymentGateway';
+export { default as ConfirmationScreen } from './ConfirmationScreen';
