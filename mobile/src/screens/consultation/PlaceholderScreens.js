5f545b8fb623433f887a562987768f847a60b028
kuldeepraj <kr9667102@gmail.com>
2026-02-07 15:50:16 +0530

    Remove PlaceholderScreens, SpecialtySelector, and SymptomsForm components to streamline consultation flow and improve maintainability.

--- mobile/src/screens/consultation/PlaceholderScreens.js
+++
@@ -1,6 +0,0 @@
-// Re-export the actual implemented components
-export { default as DocumentUpload } from './DocumentUpload';
-export { default as PersonalDetailsForm } from './PersonalDetailsForm';
-export { default as DoctorSelection } from './DoctorSelection';
-export { default as PaymentGateway } from './PaymentGateway';
-export { default as ConfirmationScreen } from './ConfirmationScreen';
