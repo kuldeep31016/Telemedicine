ff5ac932c707152557a17d09b04d763a965a7c16
kuldeepraj <kr9667102@gmail.com>
2026-02-10 11:59:11 +0530

    refactor: simplify Firebase initialization by using `initializeApp` directly and removing unused configuration properties.

--- TelemedicineApp/src/services/firebase/config.ts
+++ TelemedicineApp/src/services/firebase/config.ts
@@ -1,6 +1,6 @@
-import firebase, { FirebaseApp } from '@react-native-firebase/app';
+import { initializeApp } from '@react-native-firebase/app';
 
-// Firebase credentials from old app
+// Firebase credentials
 const firebaseConfig = {
   apiKey: 'AIzaSyA7PuNXKInMUC89_znuoxMRj7iI0m9dD5g',
   authDomain: 'telemedicine-dcfa2.firebaseapp.com',
@@ -8,17 +8,13 @@ const firebaseConfig = {
   storageBucket: 'telemedicine-dcfa2.firebasestorage.app',
   messagingSenderId: '555237969310',
   appId: '1:555237969310:web:56411f498f242f305fda8f',
-  measurementId: 'G-6BJL51SS41',
-  databaseURL: 'https://telemedicine-dcfa2-default-rtdb.firebaseio.com',
 };
 
-let app: FirebaseApp | null = null;
+let app: any = null;
 
-export const getFirebaseApp = (): FirebaseApp => {
-  if (firebase.apps.length > 0) {
-    app = firebase.app();
-  } else {
-    app = firebase.initializeApp(firebaseConfig);
+export const getFirebaseApp = () => {
+  if (!app) {
+    app = initializeApp(firebaseConfig);
   }
   return app;
 };
