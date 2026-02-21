37383e90ecd6c9a949c785428033732a8525112e
kuldeepraj <kr9667102@gmail.com>
2026-02-21 20:59:35 +0530

    fix: update axios base URL to use VITE_API_BASE_URL

--- web-react-vite/src/config/axios.js
+++ web-react-vite/src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { auth } from './firebase';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
