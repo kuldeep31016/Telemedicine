8dc9445a8e1019f8344a00b55a486950298c780c
kuldeepraj <kr9667102@gmail.com>
2026-02-25 18:38:28 +0530

    feat: add VoiceSymptoms component with VoiceSymptomAnalyzer

---
+++ web-react-vite/src/pages/Patient/VoiceSymptoms.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VoiceSymptomAnalyzer from '../../components/VoiceSymptomAnalyzer';
+
+const VoiceSymptoms = () => {
+  return (
+    <div>
+      <VoiceSymptomAnalyzer />
+    </div>
+  );
+};
+
+export default VoiceSymptoms;
